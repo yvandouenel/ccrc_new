4a48f4caebc3570244c4083767dda4c29f69b54f
Yvan Douënel <y.douenel@coopernet.fr>
2016-09-21 10:44:33 +0200

    Module inline_entity_form up for security reason.

--- sites/all/modules/inline_entity_form/inline_entity_form.api.php
+++ sites/all/modules/inline_entity_form/inline_entity_form.api.php
@@ -31,7 +31,29 @@ function hook_inline_entity_form_entity_form_alter(&$entity_form, &$form_state) 
  *   The form state of the parent form.
  */
 function hook_inline_entity_form_reference_form_alter(&$reference_form, &$form_state) {
-  $reference_form['entity_id']['description'] = t('New autocomplete description');
+  $reference_form['entity_id']['#description'] = t('New autocomplete description');
+}
+
+/**
+ * Perform alterations to the IEF field type settings.
+ *
+ * This allows modules to enable IEF to work on custom field types.
+ *
+ * @param $settings
+ *   An array with the following keys:
+ *   - entity_type - The entity_type being managed.
+ *   - bundles - Bundles of entities that the user is allowed to create.
+ *   - column - The name of the ref. field column that stores the entity id.
+ * @param $field
+ *   The field array of the reference field.
+ * @param $instance
+ *   The instance array of the reference field.
+ */
+function hook_inline_entity_form_settings_alter(&$settings, $field, $instance) {
+  if ($field['type'] == 'mycustom_reference') {
+    $settings['entity_type'] = 'mycustom';
+    $settings['column'] = 'mycustom_id';
+  }
 }
 
 /**
@@ -41,7 +63,23 @@ function hook_inline_entity_form_reference_form_alter(&$reference_form, &$form_s
  * hook_entity_property_info().
  *
  * @param $fields
- *   The fields to alter.
+ *   The table fields to alter. Each field is represented by an associative
+ *   array containing the following keys:
+ *   - type: either 'property', 'field' or 'callback' to specify how the data is
+ *     defined on the related entity.
+ *   - label: the title of the table field's column in the IEF table.
+ *   - sanitized: for 'property' type table fields, a boolean indicating whether
+ *     or not the data has already been sanitized for output; if not present or
+ *     FALSE, theme_inline_entity_form_entity_table() will sanitize the data for
+ *     output using check_plain().
+ *   - formatter: for 'field' type table fields, the machine-name of the display
+ *     formatter to use to render the field data. If this is used, then further
+ *     keys will be set in the array passed to field_view_field() as the
+ *     $display parameter. This allows any of the settings defined in the field
+ *     formatter's hook_field_formatter_info() to be used.
+ *   - weight: the sort order of the column in the IEF table.
+ *   - render_callback: for 'callback' type table fields, a function name that
+ *     returns a renderable array.
  * @param $context
  *   An array with the following keys:
  *   - parent_entity_type: The type of the parent entity.
